3783828520c397d0c5862f5668c69bfdba688c83
RohanSahana <64703711+RohanSahana@users.noreply.github.com>
2021-10-01 00:24:29 +0530

    Update index.js

--- Task1/index.js
+++ Task1/index.js
@@ -6,3 +6,11 @@ function AdityaDubey() {
     document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
     document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
 }
+
+function RohanSahana() {
+    document.querySelector('.RohanSahana-name').innerText="Rohan Sahana";
+    document.querySelector('.RohanSahana-Bio').innerText="Python and Java Developer, Machine Learning Enthusiast";
+    document.querySelector('.RohanSahana-Pic').style.backgroundImage="url('./img/RohanSahana.jpg')";
+    document.querySelector('.RohanSahana-Description').innerText="your Description";
+    document.querySelector('.RohanSahana-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/rohan-sahana-10120/";
+}
