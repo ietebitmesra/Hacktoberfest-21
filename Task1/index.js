44d4b102569e4197d3fe3e7b2c52ca867eaba4dd
Pratik Butani <pratik13butani@gmail.com>
2021-10-01 01:18:19 +0530

    Update index.js

--- Task1/index.js
+++ Task1/index.js
@@ -1,3 +1,10 @@
+function AdityaDubey() {
+    document.querySelector('.AdityaDubey-name').innerText="AdityaDubey";
+    document.querySelector('.AdityaDubey-Bio').innerText="Strive until you succeed";
+    document.querySelector('.AdityaDubey-Pic').style.backgroundImage="url('./img/AD.jpg')";
+    document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
+    document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
+}
 
 function PratikButani() {
     document.querySelector('.PratikButani-name').innerText="Pratik Butani";
