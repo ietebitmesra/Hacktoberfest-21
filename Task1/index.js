86ea4aa59c3faacba844fdfd6bcbbd583396e5e1
Pratik Butani <pratik13butani@gmail.com>
2021-10-01 01:01:34 +0530

    Update index.js

--- Task1/index.js
+++ Task1/index.js
@@ -1,8 +1,8 @@
 
-function AdityaDubey() {
-    document.querySelector('.AdityaDubey-name').innerText="AdityaDubey";
-    document.querySelector('.AdityaDubey-Bio').innerText="Strive until you succeed";
-    document.querySelector('.AdityaDubey-Pic').style.backgroundImage="url('./img/AD.jpg')";
-    document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
-    document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
+function PratikButani() {
+    document.querySelector('.PratikButani-name').innerText="Pratik Butani";
+    document.querySelector('.PratikButani-Bio').innerText="I am your HTML, You are my CSS";
+    document.querySelector('.PratikButani-Pic').style.backgroundImage="url('./img/pratikbutani.jpg')";
+    document.querySelector('.PratikButani-Description').innerText="Flutter Developer";
+    document.querySelector('.PratikButani-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/pratikbutani/";
 }
