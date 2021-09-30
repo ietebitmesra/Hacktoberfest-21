5f4ddb8fb6b930cdf177f5ec337ac83a44403679
Dhruv Patel <dhruv.time@gmail.com>
2021-10-01 01:32:40 +0530

    feat: added dhruv's portfolio
    added name, dp and linkedin profile link

--- Task1/index.js
+++ Task1/index.js
@@ -6,3 +6,11 @@ function AdityaDubey() {
     document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
     document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
 }
+
+function DearDhruv() {
+    document.querySelector('.DearDhruv-Name').innerText="Dhruv Patel";
+    document.querySelector('.DearDhruv-Bio').innerText="Tech Lead, Android Engineer, making lives better on @Instagram.";
+    document.querySelector('.DearDhruv-Pic').style.backgroundImage="url('./img/deardhruv.png')";
+    document.querySelector('.DearDhruv-Description').innerText="Tech Lead | Android Engineer";
+    document.querySelector('.DearDhruv-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/deardhruv/";
+}
