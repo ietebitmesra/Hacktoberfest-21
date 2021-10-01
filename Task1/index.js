3589f6d0eb4a0231550369a7ea76c24174983ab0
Simple Saviour <aryanchoudhary047@gmail.com>
2021-10-01 09:13:16 +0530

    added details

--- Task1/index.js
+++ Task1/index.js
@@ -1,8 +1,8 @@
 
-function AdityaDubey() {
-    document.querySelector('.AdityaDubey-name').innerText="AdityaDubey";
-    document.querySelector('.AdityaDubey-Bio').innerText="Strive until you succeed";
-    document.querySelector('.AdityaDubey-Pic').style.backgroundImage="url('./img/AD.jpg')";
-    document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
-    document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
+function Aryan() {
+    document.querySelector('.Aryan-name').innerText="Aryan";
+    document.querySelector('.Aryan-Bio').innerText="Success without failures is a mirage";
+    document.querySelector('.Aryan-Pic').style.backgroundImage="url('./img/aryan.jpg')";
+    document.querySelector('.Aryan-Description').innerText="Web Developer";
+    document.querySelector('.Aryan-Portfolio').attributes[1].nodeValue="https://aryan0472.github.io/Portfolio/";
 }
