b880b146f80797684751811f04e820373a430cd9
hritik chaudhary <hritik@hritiks-Mac.local>
2021-10-01 09:14:36 +0530

    task1-hritik

--- Task1/index.js
+++ Task1/index.js
@@ -6,3 +6,11 @@ function AdityaDubey() {
     document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
     document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
 }
+
+function HritikChaudhary() {
+    document.querySelector('.HritikChaudhary-name').innerText="Hritik Chaudhary";
+    document.querySelector('.HritikChaudhary-Bio').innerText="The best view comes after the hardest climb!";
+    document.querySelector('.HritikChaudhary-pic').style.backgroundImage="url('./img/hritik.jpg')";
+    document.querySelector('.HritikChaudhary-Description').innerText="Web Developer";
+    document.querySelector('.HritikChaudhary-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/hritik-c-0588b999/";
+}
