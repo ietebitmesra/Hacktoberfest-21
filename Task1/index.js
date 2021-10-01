b1c3c9658f0e7d12516330b67c9233d402d14b13
Simple Saviour <aryanchoudhary047@gmail.com>
2021-10-01 11:41:31 +0530

    addition of data

--- Task1/index.js
+++ Task1/index.js
@@ -15,5 +15,11 @@ function PratikButani() {
     document.querySelector('.PratikButani-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/pratikbutani/";
 }
 
-
+function Aryan() {
+    document.querySelector('.Aryan-name').innerText="Aryan";
+    document.querySelector('.Aryan-Bio').innerText="Success without failures is a mirage";
+    document.querySelector('.Aryan-Pic').style.backgroundImage="url('./img/aryan.jpg')";
+    document.querySelector('.Aryan-Description').innerText="Web Developer";
+    document.querySelector('.Aryan-Portfolio').attributes[1].nodeValue="https://aryan0472.github.io/Portfolio/";
+}
 
