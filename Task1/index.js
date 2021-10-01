125221b0a0779e8a90797720eab88439339afbdc
saketh <sakpab8@gmail.com>
2021-10-01 11:18:52 +0530

    task-1 skp

--- Task1/index.js
+++ Task1/index.js
@@ -1,19 +1,33 @@
-
 function AKD() {
-    document.querySelector('.AKD-name').innerText="Aditya Dubey";
-    document.querySelector('.AKD-Bio').innerText="Strive until you succeed !!";
-    document.querySelector('.AKD-Pic').style.backgroundImage="url('./img/AD.jpg')";
-    document.querySelector('.AKD-Description').innerText="Competitive Programmer";
-    document.querySelector('.AKD-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
+  document.querySelector(".AKD-name").innerText = "Aditya Dubey";
+  document.querySelector(".AKD-Bio").innerText = "Strive until you succeed !!";
+  document.querySelector(".AKD-Pic").style.backgroundImage =
+    "url('./img/AD.jpg')";
+  document.querySelector(".AKD-Description").innerText =
+    "Competitive Programmer";
+  document.querySelector(".AKD-Portfolio").attributes[1].nodeValue =
+    "https://www.linkedin.com/in/dubeyaditya6232/";
 }
 
 function PratikButani() {
-    document.querySelector('.PratikButani-name').innerText="Pratik Butani";
-    document.querySelector('.PratikButani-Bio').innerText="I am your HTML, You are my CSS";
-    document.querySelector('.PratikButani-Pic').style.backgroundImage="url('./img/pratikbutani.jpg')";
-    document.querySelector('.PratikButani-Description').innerText="Flutter Developer";
-    document.querySelector('.PratikButani-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/pratikbutani/";
+  document.querySelector(".PratikButani-name").innerText = "Pratik Butani";
+  document.querySelector(".PratikButani-Bio").innerText =
+    "I am your HTML, You are my CSS";
+  document.querySelector(".PratikButani-Pic").style.backgroundImage =
+    "url('./img/pratikbutani.jpg')";
+  document.querySelector(".PratikButani-Description").innerText =
+    "Flutter Developer";
+  document.querySelector(".PratikButani-Portfolio").attributes[1].nodeValue =
+    "https://www.linkedin.com/in/pratikbutani/";
 }
 
-
-
+function SKP() {
+  document.querySelector(".SKP-name").innerText = "Saketh kumar Pabba";
+  document.querySelector(".SKP-Bio").innerText =
+    "मेहनत इतनी खामोशी से करो की सफलता शोर मचा दे!";
+  document.querySelector(".SKP-Pic").style.backgroundImage =
+    "url('./img/skp.jpg')";
+  document.querySelector(".SKP-Description").innerText = "Web developer";
+  document.querySelector(".SKP-Portfolio").attributes[1].nodeValue =
+    "https://www.linkedin.com/in/saketh-pabba-783a94194/";
+}
