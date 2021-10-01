f88bbaab71b3fe53518c23a8c7008e2fea89d475
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-10-01 10:38:26 +0530

    Done with Task 1

--- Task1/index.js
+++ Task1/index.js
@@ -6,3 +6,13 @@ function AdityaDubey() {
     document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
     document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
 }
+
+// style="background-image: url('./img/SHIVAM1422000.jpg')"
+
+function SHIVAM1422000() {
+    document.querySelector('.SHIVAM1422000-name').innerText="Shivam Ojha";
+    document.querySelector('.SHIVAM1422000-Bio').innerText="Keep Debugging, Keep Growing..!!";
+    document.querySelector('.SHIVAM1422000-pic').style.backgroundImage="url('./img/SHIVAM1422000.jpg')";
+    document.querySelector('.SHIVAM1422000-Description').innerText="A Passionate Web Developer";
+    document.querySelector('.SHIVAM1422000-Portfolio').attributes[1].nodeValue="https://github.com/SHIVAM1422000";
+}
