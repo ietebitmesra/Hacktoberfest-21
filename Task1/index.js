2c74676eea595e8d98bbfd4b1e17edb074e4b383
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2021-10-03 01:33:35 +0530

    Task 1 (js)

--- Task1/index.js
+++ Task1/index.js
@@ -411,3 +411,15 @@ function MayankJha() {
   document.querySelector('.mayankJha-Description').innerText="Web Developer|Competitive Programmer";
   document.querySelector('.mayankJha-Portfolio').attributes[1].nodeValue="#";
 }
+
+function SACHIN() {
+  document.querySelector(".SACHIN-name").innerText = "SACHIN";
+  document.querySelector(".SACHIN-Bio").innerText =
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.";
+  document.querySelector(".SACHIN-Pic").style.backgroundImage =
+    "url('./img/SACHIN.jpg')";
+  document.querySelector(".SACHIN-Description").innerText =
+    "Competitive Programmer";
+  document.querySelector(".SACHIN-Portfolio").attributes[1].nodeValue =
+    "https://www.linkedin.com/in/sachin-7958a4191/";
+}
