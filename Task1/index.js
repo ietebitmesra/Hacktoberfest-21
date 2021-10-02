3e60733e76e54439c4b039fafb5936a82d668a81
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2021-10-03 01:05:26 +0530

    Task 1 (Sachin)

--- Task1/index.js
+++ Task1/index.js
@@ -6,3 +6,15 @@ function AdityaDubey() {
     document.querySelector('.AdityaDubey-Description').innerText="Competitive Programmer";
     document.querySelector('.AdityaDubey-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/dubeyaditya6232/";
 }
+
+function SACHIN() {
+  document.querySelector(".SACHIN-name").innerText = "SACHIN";
+  document.querySelector(".SACHIN-Bio").innerText =
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.";
+  document.querySelector(".SACHIN-Pic").style.backgroundImage =
+    "url('./img/SACHIN.jpg')";
+  document.querySelector(".SACHIN-Description").innerText =
+    "Competitive Programmer";
+  document.querySelector(".SACHIN-Portfolio").attributes[1].nodeValue =
+    "https://www.linkedin.com/in/sachin-7958a4191/";
+}
