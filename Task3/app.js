28bf8d3caa55ac886875a35e565452ddcc45225d
rajgohil07 <raj.famous007@gmail.com>
2021-10-04 00:38:43 +0530

    solve issue

--- Task3/app.js
+++ Task3/app.js
@@ -30,8 +30,19 @@ form.addEventListener('submit', async (e) => {
     const image = bestMatch.image.medium
     // const premeired = bestMatch.image.medium
     const name = bestMatch.name
-    const rating = "Ratings: " + bestMatch.rating.average + " ⭐️"
-    const summary = bestMatch.summary
+    const name = bestMatch.name
+
+
+    let rating = "Ratings: " + bestMatch.rating.average;
+    let starsLength = Math.floor(bestMatch.rating.average);
+    let i = starsLength - 1;
+    let j = 10 - starsLength;
+
+    for (i; i >= 0; --i)
+        rating = rating + " ⭐️";
+
+    for (j; j >= 1; --j)
+        rating = rating + " ✰"
 
 
     // CREATE DOM ELEMENTS HERE
