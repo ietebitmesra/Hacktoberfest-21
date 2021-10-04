9fe47f65be378bce84143b472e664cbcbf7e2f4d
Hritik Chaudhary <48386941+hrit-ik@users.noreply.github.com>
2021-10-04 14:23:32 +0530

    Update index.js

--- Task1/index.js
+++ Task1/index.js
@@ -299,16 +299,6 @@ function hardik() {
     "https://www.linkedin.com/in/hardikpedia/";
 }
 
-function saugatrimal() {
-    document.querySelector('.saugatrimal-name').innerText="Saugat Rimal";
-    document.querySelector('.saugatrimal-Bio').innerText="👨‍💻𝐂𝐨𝐝𝐞 𝐢𝐬 𝐏𝐨𝐞𝐭𝐫𝐲 𝐃𝐞𝐬𝐢𝐠𝐧 𝐢𝐬 𝐏𝐚𝐬𝐬𝐢𝐨𝐧";
-    document.querySelector('.saugatrimal-Pic').style.backgroundImage="url('./img/saugatrimal.jpg')";
-    document.querySelector('.saugatrimal-Description').innerText="Wordpress Developer";
-    document.querySelector('.saugatrimal-Portfolio').attributes[1].nodeValue="https://www.saugatreemal.engineer/";
-}
-
-
-
 function jhoserpacheco() {
   document.querySelector(".jhoserpacheco-name").innerText = "Jhoser Pacheco";
   document.querySelector(".jhoserpacheco-Bio").innerText =
@@ -504,4 +494,12 @@ function niraj() {
   document.querySelector('.niraj-Pic').style.backgroundImage="url('./img/niraj.png')";
   document.querySelector('.niraj-Description').innerText="Python Programmer && Problem Solver";
   document.querySelector('.niraj-Portfolio').attributes[1].nodeValue="https://www.linkedin.com/in/nirajn/";
+}
+
+function saugatrimal() {
+  document.querySelector('.saugatrimal-name').innerText="Saugat Rimal";
+  document.querySelector('.saugatrimal-Bio').innerText="👨‍💻𝐂𝐨𝐝𝐞 𝐢𝐬 𝐏𝐨𝐞𝐭𝐫𝐲 𝐃𝐞𝐬𝐢𝐠𝐧 𝐢𝐬 𝐏𝐚𝐬𝐬𝐢𝐨𝐧";
+  document.querySelector('.saugatrimal-Pic').style.backgroundImage="url('./img/saugatrimal.jpg')";
+  document.querySelector('.saugatrimal-Description').innerText="Wordpress Developer";
+  document.querySelector('.saugatrimal-Portfolio').attributes[1].nodeValue="https://www.saugatreemal.engineer/";
 }
